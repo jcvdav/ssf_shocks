38528117cd0e0286d153e4a969b131734f0a8fb5
Juan Carlos Villasenor Derbez <juancarlos@ucsb.edu>
2022-10-30 18:30:01 -0700

    Added scripts for a few figures

---
+++ scripts/03_content/02_sst_anom_map.R
@@ -0,0 +1,90 @@
+################################################################################
+# title
+################################################################################
+#
+# Juan Carlos Villaseñor-Derbez
+# juancvd@stanford.edu
+# date
+#
+# Description
+#
+################################################################################
+
+## SET UP ######################################################################
+
+# Load packages ----------------------------------------------------------------
+library(here)
+library(rnaturalearth)
+library(raster)
+library(sf)
+library(tidyverse)
+
+# Load data --------------------------------------------------------------------
+sst_stack <- list.files(
+  path = here::here("../data_remotes/data/sst/processed_annual"),
+  pattern = "tif",
+  full.names = T)[c(8, 13)] %>%
+  stack()
+
+coast <-
+  ne_countries(country = c("Mexico", "United States of America"),
+               returnclas = "sf", scale = "medium") %>% 
+  st_transform(crs = "EPSG:4326") %>% 
+  st_crop(sst_stack)
+
+turfs <-
+  sf::st_read(dsn = file.path(
+    mex_data_path,
+    "concesiones",
+    "processed",
+    "lobster_turf_polygons.gpkg"
+  )) %>%
+  st_transform(crs = 4326)
+
+
+## PROCESSING ##################################################################
+
+# X ----------------------------------------------------------------------------
+
+dif <- (sst_stack[[2]] - sst_stack[[1]]) %>% 
+  raster::aggregate(fact = 10) %>% 
+  as.data.frame(xy = T) %>% 
+  drop_na()
+
+## VISUALIZE ###################################################################
+
+# X ----------------------------------------------------------------------------
+
+sst_anom <- ggplot() +
+  geom_raster(data = dif,
+              mapping = aes(x = x, y = y, fill = layer)) +
+  geom_sf(data = coast, color = "black", size = 0.5) + 
+  scale_fill_gradientn(colors = colorRamps::blue2red(10)) +
+  labs(x = "",
+       y = "",
+       fill = "SST anomaly\n(°C)",
+       title = "Difference in SST",
+       subtitle = bquote(SST[2015]-SST[2010])) +
+  scale_x_continuous(expand = c(0, 0)) +
+  scale_y_continuous(expand = c(0, 0)) +
+  theme_minimal() +
+  theme(legend.justification = c(1, 1),
+        legend.position = c(1, 1)) +
+  guides(fill = guide_colorbar(frame.colour = "black",
+                               ticks.colour = "black"))
+
+sst_anom_turfs <- sst_anom +
+  geom_sf(data = turfs, color = "black", fill = "transparent")
+
+## EXPORT ######################################################################
+
+# X ----------------------------------------------------------------------------
+startR::lazy_ggsave(plot = sst_anom,
+                    filename = "sst_anom",
+                    width = 15,
+                    height = 15)
+
+startR::lazy_ggsave(plot = sst_anom_turfs,
+                    filename = "sst_anom_turfs",
+                    width = 15,
+                    height = 15)
