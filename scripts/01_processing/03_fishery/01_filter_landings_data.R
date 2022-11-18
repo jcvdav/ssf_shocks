2053ab136b0dd1de40ae001e2e44006489f016c9
Juan Carlos Villasenor Derbez <juancarlos@ucsb.edu>
2022-11-18 11:57:50 -0800

    Added code and data for fishery panel

--- scripts/01_processing/03_fishery/01_filter_landings_data.R
+++ scripts/01_processing/03_fishery/01_filter_landings_data.R
@@ -40,17 +40,39 @@ spp <- c(
 
 # X ----------------------------------------------------------------------------
 filtered <- landings %>%
+  filter(main_species_group %in% spp,
+         between(year, 2003, 2021) # Most permits were granted in 2002
+         # year < 2022
+         ) %>% 
   inner_join(coop_eurnpa, by = "eu_rnpa") %>%
-  filter(main_species_group %in% spp)
+  left_join(periods, by = "year") %>% 
+  group_by(coop_name, main_species_group) %>% 
+  mutate(n = n_distinct(period)) %>% 
+  ungroup() %>% 
+  filter(n == 3) %>% 
+  group_by(coop_name, main_species_group, period) %>% 
+  mutate(n2 = n()) %>% 
+  ungroup() %>% 
+  filter(n2 > 3) %>%
+  select(-c(n, n2)) %>% 
+  group_by(coop_name, main_species_group) %>% 
+  mutate(norm_landed_weight = (landed_weight - mean(landed_weight)) / sd(landed_weight),
+         norm_value = (value - mean(value)) / sd(value)) %>%
+  ungroup() %>% 
+  mutate(balanced = year >= 2008) %>% 
+  select(coop_name,
+         eu_rnpa,
+         year,
+         balanced,
+         main_species_group,
+         landed_weight, value,
+         norm_landed_weight, norm_value)
+  
   
 
 ## EXPORT ######################################################################
 
 # X ----------------------------------------------------------------------------
 saveRDS(object = filtered,
-        file = here("data", "processed", "filtered_landings.rds"))
+        file = here("data", "processed", "annual_fishery_panel.rds"))
 
-
-TO DO
-- Filter landings data: why am I grouping and summarizing?
-- Build fishery estimation panel
