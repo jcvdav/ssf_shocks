841359c0e995f8ffe838fd32f5f5cc131fae7377
Juan Carlos Villasenor Derbez <juancarlos@ucsb.edu>
2022-11-18 11:52:25 -0800

    Renamed and fixed script to make env ecol panel

---
+++ scripts/01_processing/04_make_estimation_panels/01_make_ecol_env.R
@@ -0,0 +1,34 @@
+################################################################################
+# title
+################################################################################
+#
+# Juan Carlos Villaseñor-Derbez
+# juancvd@stanford.edu
+# date
+#
+# Description
+#
+################################################################################
+
+## SET UP ######################################################################
+
+# Load packages ----------------------------------------------------------------
+library(here)
+library(tidyverse)
+
+# Load data --------------------------------------------------------------------
+env_panel <- readRDS(here("data", "processed", "annual_environmental_panel.rds"))
+eco_panel <- readRDS(here("data", "processed", "annual_ecological_panel.rds"))
+
+## PROCESSING ##################################################################
+
+# Combine ----------------------------------------------------------------------
+annual_env_eco_panel <- eco_panel %>% 
+  left_join(env_panel, by = c("year", "coop_name"))
+
+
+## EXPORT ######################################################################
+
+# Export the panel  ------------------------------------------------------------
+saveRDS(object = annual_env_eco_panel,
+        file = here("data", "estimation_panels", "env_eco_panel.rds"))
