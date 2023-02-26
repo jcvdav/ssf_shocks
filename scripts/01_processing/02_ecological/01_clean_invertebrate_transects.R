3b8ae25e8988a1390fd1f3dd8a4bded0fa46c86b
Juan Carlos Villasenor Derbez <juancarlos@ucsb.edu>
2023-02-25 22:23:55 -0800

    Updated invert script

--- scripts/01_processing/02_ecological/01_clean_invertebrate_transects.R
+++ scripts/01_processing/02_ecological/01_clean_invertebrate_transects.R
@@ -17,6 +17,10 @@ library(here)
 library(readxl)
 library(tidyverse)
 
+std_normal <- function(x, year, cutoff_year = 2013) {
+  (x - mean(x[year <= cutoff_year], na.rm = T)) / sd(x[year <= cutoff_year], na.rm = T)
+}
+
 # Load data --------------------------------------------------------------------
 cobi <- read_excel(
   path = here("data",
@@ -28,22 +32,42 @@ cobi <- read_excel(
 
 # X ----------------------------------------------------------------------------
 cobi_clean <- cobi %>%
-  filter(ZONA == "Reserva") %>% 
+  filter(ZONA == "Reserva",
+         COMUNIDAD == "Isla Natividad") %>% 
   select(year = ANIO,
          community = COMUNIDAD,
          site = SITIO,
          depth_m = PROFUNDIDAD_INICIAL,
          transect = Transecto,
-         genus = GENERO,
          species = ESPECIE,
          abundance = ABUNDANCIA) %>% 
   filter(species %in% c("Panulirus interruptus",
                         "Parastichopus parvimensis",
-                        "Mesocentrotus franciscanus"),
-         community %in% c("Isla Natividad", "El Rosario")) %>% 
+                        "Mesocentrotus franciscanus")) %>% 
+  complete(year, community, site, species, fill = list(transect = 0,
+                                            abundance = 0)) %>%
   mutate(density = abundance / 60,
-         coop_name = case_when(community == "Isla Natividad" ~ "Buzos y Pescadores",
-                               community == "El Rosario" ~ "Ensenada"))
+         eu_rnpa = "0301000089",
+         fishery = case_when(
+           species == "Panulirus interruptus" ~ "lobster",
+           species == "Parastichopus parvimensis" ~ "sea_cucumber",
+           species == "Mesocentrotus franciscanus" ~ "urchin",
+         )) %>% 
+  left_join(periods, by = "year") %>% 
+  group_by(eu_rnpa, fishery, site) %>% 
+  mutate(n_periods = n_distinct(period)) %>% 
+  ungroup() %>% 
+  filter(n_periods == 3) %>% 
+  group_by(site, eu_rnpa, fishery, period) %>% 
+  mutate(n_years = n_distinct(year)) %>% 
+  ungroup() %>% 
+  group_by(site, eu_rnpa, fishery) %>% 
+  filter(all(n_years >= 3)) %>%
+  select(-c(n_periods, n_years)) %>%
+  group_by(eu_rnpa, site, fishery) %>% 
+  mutate(norm_abundance = std_normal(x = abundance, y = year),
+         norm_density = std_normal(x = density, y = year)) %>% 
+  ungroup() 
 
 ## EXPORT ######################################################################
 
@@ -51,3 +75,4 @@ cobi_clean <- cobi %>%
 
 saveRDS(object = cobi_clean,
         file = here("data", "processed", "annual_ecological_panel.rds"))
+
