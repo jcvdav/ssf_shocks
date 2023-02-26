6e6d42e2e446c2e5cc301c314f9e85fe90644c72
Juan Carlos Villasenor Derbez <juancarlos@ucsb.edu>
2023-02-25 22:29:23 -0800

    Started script to analuze the frequency of past MHW

--- scripts/02_analysis/MHW_frequency.R
+++ scripts/02_analysis/MHW_frequency.R
@@ -24,16 +24,20 @@
 ## SET UP ######################################################################
 
 # Load packages ----------------------------------------------------------------
+library(here)
+library(tidyverse)
 
 # Load data --------------------------------------------------------------------
 mhw <- readRDS(file = here("data", "processed", "mhw_by_turf.rds"))
 
+baseline <- readRDS(here("data", "estimation_panels", "env_fish_panel.rds")) %>%
+  select(eu_rnpa, fishery) %>%
+  distinct()
+
+
 ## PROCESSING ##################################################################
 
 # X ----------------------------------------------------------------------------
-
-mhw$summary[[1]]$mhw_events
-
 mhw %>% 
   select(eu_rnpa, summary) %>% 
   unnest(summary) %>% 
@@ -61,6 +65,52 @@ mhw %>%
 ## VISUALIZE ###################################################################
 
 # X ----------------------------------------------------------------------------
+
+get_cond_p <- function(threshold, data){
+  # browser()
+  n_at_or_above_threshold <- sum(data$intensity_cumulative >= threshold)
+  cond_p <- n_at_or_above_threshold / length(data$intensity_cumulative)
+  
+  back <- tibble(threshold,
+                 cond_p)
+  
+  return(back)
+}
+
+mhw_by_turf <- mhw %>% 
+  inner_join(baseline, by = c("eu_rnpa", "fishery")) %>% 
+  mutate(mhw = map(mhw, ~.x$event)) %>% 
+  select(eu_rnpa, fishery, mhw) %>% 
+  expand_grid(threshold = seq(1, 900, by = 5)) %>% 
+  mutate(cond_p = map2(.x = threshold, .y = mhw, .f = ~get_cond_p(.x, .y)))
+
+mhw_by_turf %>% 
+  select(fishery, eu_rnpa, cond_p) %>%
+  unnest(cond_p) %>%
+  ggplot(aes(x = threshold, y = cond_p, group = eu_rnpa)) +
+  geom_line() +
+  facet_wrap(~fishery, ncol = 2)
+
+
+
+
+events_by_fishery <- mhw %>% 
+  inner_join(baseline, by = c("eu_rnpa", "fishery")) %>% 
+  mutate(climatology = map(mhw, ~.x$climatology)) %>% 
+  select(eu_rnpa, fishery, climatology) %>% 
+  unnest(climatology) %>% 
+  mutate(year = lubridate::year(t)) %>% 
+  group_by(eu_rnpa, fishery, year) %>% 
+  summarize(at_least_one_mhw = any(event)) %>% 
+  ungroup() %>% 
+  group_by(eu_rnpa, fishery) %>% 
+  summarize(p = sum(at_least_one_mhw) / n_distinct(year)) %>% 
+  ungroup()
+
+events_by_fishery %>% 
+  group_by(fishery) %>%
+  summarize(p_m = mean(p),
+            p_sd = sd(p))
 
 ## EXPORT ######################################################################
 
